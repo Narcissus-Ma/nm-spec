0bc38da3d05d3201925eaf72fdee8f2a589bdb0b
Narcissus-Ma <103873240+Narcissus-Ma@users.noreply.github.com>
2023-11-17 08:05:15 +0000

    Deploying to gh-pages from @ Narcissus-Ma/nm-spec@e775b8f7caf3a9f035f8e5c68c679f95ef488736 🚀

--- packages/eslint-plugin/configs/recommended.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: ['narcissus-fe-eslint-plugin'],
-  rules: {
-    'narcissus-fe-eslint-plugin/no-http-url': 'warn',
-    'narcissus-fe-eslint-plugin/no-secret-info': 'error',
-  },
-};
--- packages/narcissus-fe-lint/.eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['narcissus-fe-eslint-config/typescript/node', 'prettier'],
-  rules: {
-    '@typescript-eslint/no-require-imports': 0,
-    'no-console': 0,
-  },
-};
